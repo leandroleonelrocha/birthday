962ca717a641a9ec57864924a9bd0baa265f62a9
Rocha Leandro <llrocha@desarrollosocial.gob.ar>
2021-08-26 20:29:37 -0300

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+
+import firebase from 'firebase/app';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAohV015mSVez9gHVhrcXaA7S5VLgq8Ezc',
+  authDomain: 'cumpleanos-e5447.firebaseapp.com',
+  databaseURL: 'https://cumpleanos-e5447.firebaseio.com',
+  projectId: 'cumpleanos-e5447',
+  storageBucket: 'cumpleanos-e5447.appspot.com',
+  messagingSenderId: '283606448639',
+  appId: '1:283606448639:web:b8318fbb303bd7a86d0fca',
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
